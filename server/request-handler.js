aa98db9b790b865020b8e8b6b58fd172ac40dfe0
kahlstorf1 <dkahlstorf1@gmail.com>
2023-01-07 15:54:51 -0500

    Complete Bare Minimum Requirements

--- server/request-handler.js
+++ server/request-handler.js
@@ -20,7 +20,7 @@ var url = require('url');
 // Your chat client is running from a url like file://your/chat/client/index.html,
 // which is considered a different domain.
 //
-// Another way to get around this restriction is to serve you chat
+// Another way to get around this restriction is to serve your chat
 // client from this domain by setting up static file serving.
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
@@ -34,42 +34,17 @@ var body = [];
 var requestHandler = function(request, response) {
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
-  // The outgoing status.
   var statusCode = 200;
 
-  // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  //headers['Content-Type'] = 'application/json';
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  //response.writeHead(statusCode, headers);
-
-  // response.on()
-  // response.write(data);
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-
-  //change object that is being stringified
-  //response.end(JSON.stringify(body));
-
+  if (!(headers in request)) {
+    headers['User-Agent'] = 'test';
+  }
   if (request.method === 'OPTIONS') {
-    headers = defaultCorsHeaders;
+    statusCode = 204;
   } else if (request.method === 'GET' && request.url === '/classes/messages') {
     statusCode = 200;
     headers['Content-Type'] = 'application/json';
-    //response.end(JSON.stringify(body));
   } else if (request.method === 'POST' && request.url === '/classes/messages') {
     statusCode = 201;
 
@@ -86,19 +61,14 @@ var requestHandler = function(request, response) {
       }
     });
     request.on('end', function() {
-      // response.end(JSON.stringify(body));
     });
   } else if (request.method === 'DELETE') {
     statusCode = 405;
-    //response.end();
   } else {
     statusCode = 404;
-    // response.writeHead(404, {'Content-Type': 'application/json'});
-    // response.end();
   }
   response.writeHead(statusCode, headers);
   response.end(JSON.stringify(body));
-
 };
 
 module.exports = {requestHandler, defaultCorsHeaders};

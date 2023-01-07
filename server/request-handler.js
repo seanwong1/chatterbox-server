667f1d26295c451c47de8de7ab1ca11b3c7b7c4d
kahlstorf1 <dkahlstorf1@gmail.com>
2023-01-07 14:44:52 -0500

    Complete connecting client to server

--- server/request-handler.js
+++ server/request-handler.js
@@ -13,57 +13,32 @@ this file and include it in basic-server.js so that it actually works.
 **************************************************************/
 var url = require('url');
 
+// These headers will allow Cross-Origin Resource Sharing (CORS).
+// This code allows this server to talk to websites that
+// are on different domains, for instance, your chat client.
+//
+// Your chat client is running from a url like file://your/chat/client/index.html,
+// which is considered a different domain.
+//
+// Another way to get around this restriction is to serve you chat
+// client from this domain by setting up static file serving.
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept, authorization',
+  'access-control-max-age': 10 // Seconds.
+};
+
 var body = [];
 
 var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
-  if (request.method === 'GET' && request.url === '/classes/messages') {
-    response.writeHead(200, {'Content-Type': 'application/json'});
-    response.end(JSON.stringify(body));
-  } else if (request.method === 'POST' && request.url === '/classes/messages') {
-    request.on('data', (message) => {
-      try {
-        var parseMsg = JSON.parse(message);
-        if (parseMsg.text === '' || parseMsg.username === '') {
-          throw new Error;
-        } else {
-          body.push(parseMsg);
-        }
-      } catch (error) {
-        console.error('e', error);
-      }
-    });
-    request.on('end', function() {
-      response.writeHead(201, {'Content-Type': 'application/json'});
-      response.end(JSON.stringify(body));
-    });
-  } else if (request.method === 'DELETE') {
-    response.writeHead(405, {'Content-Type': 'application/json'});
-    response.end();
-  } else {
-    response.writeHead(404, {'Content-Type': 'application/json'});
-    response.end();
-  }
-
   // The outgoing status.
+  var statusCode = 200;
 
   // See the note below about CORS headers.
-  //var headers = defaultCorsHeaders;
+  var headers = defaultCorsHeaders;
 
   // Tell the client we are sending them plain text.
   //
@@ -88,22 +63,42 @@ var requestHandler = function(request, response) {
 
   //change object that is being stringified
   //response.end(JSON.stringify(body));
-};
 
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept, authorization',
-  'access-control-max-age': 10 // Seconds.
+  if (request.method === 'OPTIONS') {
+    headers = defaultCorsHeaders;
+  } else if (request.method === 'GET' && request.url === '/classes/messages') {
+    statusCode = 200;
+    headers['Content-Type'] = 'application/json';
+    //response.end(JSON.stringify(body));
+  } else if (request.method === 'POST' && request.url === '/classes/messages') {
+    statusCode = 201;
+
+    request.on('data', (message) => {
+      try {
+        var parseMsg = JSON.parse(message);
+        if (parseMsg.text === '' || parseMsg.username === '') {
+          throw new Error;
+        } else {
+          body.push(parseMsg);
+        }
+      } catch (error) {
+        console.error('e', error);
+      }
+    });
+    request.on('end', function() {
+      // response.end(JSON.stringify(body));
+    });
+  } else if (request.method === 'DELETE') {
+    statusCode = 405;
+    //response.end();
+  } else {
+    statusCode = 404;
+    // response.writeHead(404, {'Content-Type': 'application/json'});
+    // response.end();
+  }
+  response.writeHead(statusCode, headers);
+  response.end(JSON.stringify(body));
+
 };
 
 module.exports = {requestHandler, defaultCorsHeaders};
